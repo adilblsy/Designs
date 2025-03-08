7b3f20976df665513d90f8b4015cdae5626ef9e3
adilblsy <itzmeadilerm@gmail.com>
2025-03-08 21:08:17 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+document.getElementById("toggleButton").addEventListener("click", function() {
+    let floatingBar = document.getElementById("floatingBar");
+    floatingBar.style.display = (floatingBar.style.display === "block") ? "none" : "block";
+});
+
+document.addEventListener("contextmenu", function(event) {
+    event.preventDefault();
+});
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker.register("/sw.js")
+        .then(() => console.log("Service Worker Registered"))
+        .catch((error) => console.log("Service Worker Registration Failed:", error));
+    });
+  }  
