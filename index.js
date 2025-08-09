7a7e2fbd8294e3b4995d16d4599181cb41c0710d
adilblsy <itzmeadilerm@gmail.com>
2025-08-09 15:24:23 +0530

    new

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ document.addEventListener("contextmenu", function(event) {
 
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
-      navigator.serviceWorker.register("/sw.js")
-        .then(() => console.log("Service Worker Registered"))
-        .catch((error) => console.log("Service Worker Registration Failed:", error));
+        navigator.serviceWorker.register("/sw.js")
+            .then(() => console.log("Service Worker Registered"))
+            .catch((error) => console.log("Service Worker Registration Failed:", error));
     });
-  }  
+}
+
+// Wait until all images are loaded before showing gallery
+window.addEventListener("load", function() {
+    document.getElementById("loader").style.display = "none";
+    document.querySelector(".gallery").style.display = "grid";
+});
