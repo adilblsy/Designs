26d27ef81213db61e33142ac79ec3e60f22addc0
adilblsy <itzmeadilerm@gmail.com>
2025-08-09 16:06:08 +0530

    load

--- index.js
+++ index.js
@@ -14,20 +14,3 @@ if ("serviceWorker" in navigator) {
         .catch((error) => console.log("Service Worker Registration Failed:", error));
     });
   }
-
-const backToTopBtn = document.getElementById("backToTop");
-
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
-        backToTopBtn.style.display = "block";
-    } else {
-        backToTopBtn.style.display = "none";
-    }
-});
-
-backToTopBtn.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-});
